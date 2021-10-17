2e575d8e399ecd3f5e0c2f5b4103c3d97f736e98
timothyhsu8 <timothy.hsu@stonybrook.edu>
2021-10-16 21:31:17 -0400

    Finished basic layout of the quiz taking page

--- client/src/pages/QuizTakingPage.js
+++ client/src/pages/QuizTakingPage.js
@@ -1,37 +1,74 @@
-import { Box, Center, Text, Grid, VStack, Button } from "@chakra-ui/react"
+import { Box, Center, Text, Grid, VStack, Button, Image } from "@chakra-ui/react"
 
 export default function QuizTakingPage( {} ) {
 	let question = "1. What is the song that plays at Whomp's Fortress?"
     let choices = ["Choice 1", "Choice 2", "Choice 3", "Choice 4"]
+	let questions = []
+	for (let i = 0; i < 20; i++)
+		questions.push("Question" + i+1)
 
     return (
 		<Box>
-			<Grid templateColumns="1fr 6fr"> 
+			<Grid templateColumns="1fr 6fr">
 				{/* SIDEBAR */}
-				<Box h="100vh" bgColor="gray.200" ></Box>
+				<Box h="100vh" bgColor="gray.200">
+					{/* QUIZ ICON */}
+					<Image src="https://yt3.ggpht.com/ytc/AKedOLQ2xNBI8aO1I9etug8WnhQ-WPhnVEyNgj6cFVPfNw=s900-c-k-c0x00ffffff-no-rj" />
+
+					{/* QUIZ TITLE */}
+					<Center>
+						<Text fontSize="30">
+							Nintendo Music
+						</Text>
+					</Center>
+
+					{/* QUIZ AUTHOR */}
+					<Center>
+						<Text fontSize="20">
+							MarioGamer100
+						</Text>
+					</Center>
+					
+					<Center>
+						<Box w="95%" h="0.2vh" bgColor="gray.400"/>
+					</Center>
+
+					{/* QUESTION NUMBERS */}
+					<Grid w="100%" templateColumns="1fr 1fr">
+						{questions.map((item, index)=>{
+							return (
+								<Button bgColor="gray.200"> 
+									{index+1} 
+								</Button>
+								)
+							})
+						}
+					</Grid>
+				</Box>
 
 				{/* MAIN PAGE */}
 				<Box>
 					{/* QUESTION */}
-					<Text fontSize="50">
+					<Text pt="50" fontSize="50">
 						<Center>
 							{question}
 						</Center>
 					</Text>
 
 					{/* ANSWER CHOICES */}
-					<VStack>
+					<VStack pt="30" spacing="6">
 						{choices.map((item, index)=>{
 							return (
-							<Button w="40%" h="10vh" bgColor="gray.600" fontSize="24" textColor="white"> 
+							<Button w="60%" h="10vh" bgColor="gray.600" fontSize="28" textColor="white"> 
 								{choices[index]} 
 							</Button>
 							)
 						})}
 					</VStack>
 					
-					<Center>
-						<Button w="20%" bgColor="purple.800" textColor="white">
+					{/* NEXT QUESTION BUTTON */}
+					<Center pt="20">
+						<Button w="20%" h="7vh" bgColor="purple.800" fontSize="21" textColor="white">
 							Next Question
 						</Button>
 					</Center>
